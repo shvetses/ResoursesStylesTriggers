f6ab7797aa73760b492a8930a47c24a07b32822d
Екатерина Швец <shvetses@pik.ru>
2025-07-17 18:54:09 +0300

    fix

--- Example1/MainWindow.xaml.cs
+++ Example1/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Example1
 {
@@ -23,7 +15,7 @@ namespace Example1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Resources["PrimaryBrush"]=new SolidColorBrush(Colors.Red);
+            this.Resources["PrimaryBrush"]=new SolidColorBrush(Colors.Blue);
         }
     }
 }
